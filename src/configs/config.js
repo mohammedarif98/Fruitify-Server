6284fa891a36a3b59ddeaa86bce0325061bf09bc
mohammed <arifcsidhik@gmail.com>
2025-06-28 22:25:32 +0530

    minor changes in config.js file

--- src/configs/config.js
+++ src/configs/config.js
@@ -19,7 +19,7 @@ const config = {
     },
 
     cors: {                                   
-        origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+        origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
